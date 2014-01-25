61d8b2f82397c9a570e1b18729b581884a87369b
Marc Haemmerle <mail@marchaemmerle.com>
2014-01-25 16:37:02 +0100

    prepare for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject registry "0.1.0-SNAPSHOT"
   :description "Simple registry based on ZooKeeper"
-  :url "http://github.com/mhaemmerle/registry"
+  :url "https://github.com/mhaemmerle/registry"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/mhaemmerle/registry"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.slf4j/slf4j-api "1.6.6"]
                  [org.slf4j/slf4j-log4j12 "1.6.6"]
@@ -10,5 +14,4 @@
   :dev-dependencies [[criterium "0.2.1"]]
   :plugins [[lein-marginalia "0.7.1"]]
   :jvm-opts ["-server"
-             "-Djava.awt.headless=true"]
-  :main grimoire.core)
+             "-Djava.awt.headless=true"])
